e81ff592af0cf462ed655fad655a00aa84563004
programmer04 <jakub.warczarek@gmail.com>
2019-10-22 18:38:39 +0200

    Improve style in script

--- migrate.sh
+++ migrate.sh
@@ -4,16 +4,16 @@
 # https://dustymabe.com/2015/07/14/fedora-btrfssnapper-part-1-system-preparation/
 set -ex
 
-DRIVE=/dev/sda
-LVM_VG=vgroot
-LVM_ROOT_PARTITION_LV=lvroot
+readonly DRIVE='/dev/sda'
+readonly LVM_VG='vgroot'
+readonly LVM_ROOT_PARTITION_LV='lvroot'
 
 if [ -d /sys/firmware/efi ]; then
-  echo UEFI installation
-  UEFI=true
+  echo 'UEFI installation'
+  UEFI='true'
 else
-  echo BIOS installation
-  UEFI=false
+  echo 'BIOS installation'
+  UEFI='false'
 fi
 # gdisk -l $DRIVE | grep "using GPT"
 
@@ -21,27 +21,27 @@ mount --bind / /mnt
 cp -a /boot/* /mnt/boot/
 cp -a /boot/.vmlinuz-* /mnt/boot/
 diff -ur /boot/ /mnt/boot/
-if $UEFI; then
+if "${UEFI}"; then
   rm -rf /mnt/boot/efi
 fi
 umount /mnt
 
-BOOT_PARTITION=$(mount -l | grep /boot | grep -v /boot/efi | awk '{ print $1 }')
-BOOT_PARTITION_NO=$(echo "${BOOT_PARTITION#$DRIVE}")
+BOOT_PARTITION="$(mount -l | grep /boot | grep -v /boot/efi | awk '{ print $1 }')"
+BOOT_PARTITION_NO="${BOOT_PARTITION#$DRIVE}"
 
-if $UEFI; then
-  BOOT_EFI_PARTITION=$(mount -l | grep /boot/efi | awk '{ print $1 }')
+if "${UEFI}"; then
+  BOOT_EFI_PARTITION="$(mount -l | grep /boot/efi | awk '{ print $1 }')"
 fi
 
-echo wiping /boot partition $BOOT_PARTITION, partition number $BOOT_PARTITION_NO with random data
+echo "wiping /boot partition ${BOOT_PARTITION}, partition number ${BOOT_PARTITION_NO} with random data"
 
-read -p "Continue (y/n)?" choice
-case "$choice" in 
+read -rp 'Continue (y/n)?' choice
+case "${choice}" in
   y|Y ) ;;
-  * ) echo "bailing out"; exit 1;
+  * ) echo 'bailing out'; exit 1;
 esac
 
-if $UEFI; then
+if "${UEFI}"; then
   umount /boot/efi
 fi
 umount /boot
@@ -49,22 +49,22 @@ umount /boot
 # TODO find a better way that doesn't ignore the exit code
 # TODO sync after each time it finishes
 # TODO override with encrypted random data
-seq 3 | xargs -I @ dd if=/dev/urandom of=$BOOT_PARTITION status=progress || true
+seq 3 | xargs -I @ dd if=/dev/urandom of="${BOOT_PARTITION}" status=progress || true
 
 sync
 
 fdisk /dev/sda <<EOF
 p
 d
-$BOOT_PARTITION_NO
+${BOOT_PARTITION_NO}
 w
 EOF
 # Failed to remove partition3 from system: Device or resource busy
 # kernel still uses the old partitions. The new table will be used at the next reboot.
 
-if $UEFI; then
+if "${UEFI}"; then
   mkdir /boot/efi
-  mount $BOOT_EFI_PARTITION /boot/efi
+  mount "${BOOT_EFI_PARTITION}" /boot/efi
 fi
 
 cp /etc/fstab /etc/fstab.backup
@@ -78,21 +78,21 @@ dnf install -y snapper python3-dnf-plugins-extras-snapper
 setenforce 0
 snapper --config=root create-config /
 setenforce 1
-echo "/dev/$LVM_VG/$LVM_ROOT_PARTITION_LV /.snapshots btrfs subvol=.snapshots 0 0" >> /etc/fstab
+echo "/dev/${LVM_VG}/${LVM_ROOT_PARTITION_LV} /.snapshots btrfs subvol=.snapshots 0 0" >> /etc/fstab
 
-echo GRUB_ENABLE_CRYPTODISK=y >> /etc/default/grub
-echo SUSE_BTRFS_SNAPSHOT_BOOTING=true >> /etc/default/grub
+echo GRUB_ENABLE_CRYPTODISK='y' >> /etc/default/grub
+echo SUSE_BTRFS_SNAPSHOT_BOOTING='true' >> /etc/default/grub
 
-if $UEFI; then
+if "${UEFI}"; then
   dnf install -y grub2-efi-x64-modules
   grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
   grub2-install --efi-directory=/boot/efi --boot-directory=/boot/efi/EFI/fedora --debug
 else
   grub2-mkconfig -o /boot/grub2/grub.cfg
-  grub2-install $DRIVE
+  grub2-install "${DRIVE}"
 fi
 
 # new kernel install creates a new entry in /boot/loader/entries with correct vmlinuz path (with /boot prefix)
 dnf update -y kernel
 
-echo Ready to reboot
+echo 'Ready to reboot'
